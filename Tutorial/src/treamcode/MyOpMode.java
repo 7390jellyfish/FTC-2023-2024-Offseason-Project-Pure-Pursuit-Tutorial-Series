1a765eb72167dfa9607d2126a9d41912ae4d1474
hey000hey <alanrong1@gmail.com>
2024-04-19 21:58:50 -0700

    Added skeleton motor code

--- Tutorial/src/treamcode/MyOpMode.java
+++ Tutorial/src/treamcode/MyOpMode.java
@@ -8,44 +8,23 @@ import static treamcode.RobotMovement.followCurve;
 
 public class MyOpMode extends OpMode {
 
-    ArrayList<CurvePoint> allPoints = new ArrayList<>();
     @Override
     public void init() {
         // Add initialization code here
         System.out.println("Initializing MyOpMode...");
-        allPoints.add(new CurvePoint(0, 0, 1, 1.0, 50, Math.toRadians(50), 1.0 ));
-        allPoints.add(new CurvePoint(180, 180, 1 , 1.0, 50, Math.toRadians(50), 1.0 ));
-        allPoints.add(new CurvePoint(220, 180, 1, 1.0, 50, Math.toRadians(50), 1.0 ));
-        allPoints.add(new CurvePoint(280, 50, 1, 1.0, 10, Math.toRadians(50), 1.0 ));
-        allPoints.add(new CurvePoint(180, 10, 1, 1.0, 10, Math.toRadians(50), 1.0 ));
-
-
-        CurvePoint secondLastPoint = allPoints.get(allPoints.size() - 2);
-        CurvePoint lastPoint = allPoints.getLast();
-        double m = (lastPoint.y - secondLastPoint.y) / (lastPoint.x - secondLastPoint.x);
-
-        CurvePoint extend = lastPoint;
-        if ((secondLastPoint.x - lastPoint.x) < 0) {
-            extend.x += 30;
-            extend.y += ((m * extend.x) + (m * lastPoint.x));
-        } else if ((secondLastPoint.x - lastPoint.x) > 0 ) {
-            extend.x -= 30;
-            extend.y += ((m * extend.x) + (m * lastPoint.x));
-        } else if ((secondLastPoint.y - lastPoint.y) < 0) {
-            extend.y += 30;
-        } else {
-            extend.y -= 30;
-        }
-
-        allPoints.add(extend);
-        System.out.println("LAST POINT.x: " + allPoints.getLast().x);
-        System.out.println("LAST POINT.y: " + allPoints.getLast().y);
     }
 
     @Override
     public void loop() {
         // Add main loop code here
         System.out.println("Initialized");
+        ArrayList<CurvePoint> allPoints = new ArrayList<>();
+        allPoints.add(new CurvePoint(0, 0, 1.0, 1.0, 50, Math.toRadians(50), 1.0 ));
+        allPoints.add(new CurvePoint(180, 180, 1.0, 1.0, 50, Math.toRadians(50), 1.0 ));
+        allPoints.add(new CurvePoint(220, 180, 1.0, 1.0, 50, Math.toRadians(50), 1.0 ));
+        allPoints.add(new CurvePoint(280, 50, 1.0, 1.0, 50, Math.toRadians(50), 1.0 ));
+        allPoints.add(new CurvePoint(180, 0, 1.0, 1.0, 50, Math.toRadians(50), 1.0 ));
+
         followCurve(allPoints,Math.toRadians(90));
     }
 
--- Tutorial/src/treamcode/RobotMovement.java
+++ Tutorial/src/treamcode/RobotMovement.java
@@ -9,7 +9,6 @@ import org.opencv.core.Point;
 
 import java.util.ArrayList;
 
-import static treamcode.MyOpMode.*;
 import static com.company.Robot.*;
 import static RobotUtilities.MovementVars.*;
 import static treamcode.MathFunctions.lineCircleIntersection;
@@ -19,7 +18,6 @@ public class RobotMovement {
 
 
     public static void followCurve(ArrayList<CurvePoint> allPoints, double followAngle) {
-
         for(int i = 0; i < allPoints.size() - 1; i++){
             ComputerDebugging.sendLine(new FloatPoint(allPoints.get(i).x,allPoints.get(i).y), new FloatPoint(allPoints.get(i+1).x,allPoints.get(i+1).y));
         }
@@ -27,9 +25,7 @@ public class RobotMovement {
 
         ComputerDebugging.sendKeyPoint(new FloatPoint(followMe.x, followMe.y));
 
-//        if(!((worldXPosition - allPoints.get(allPoints.size() - 2).x) < 0.1) || !((worldYPosition - allPoints.get(allPoints.size() - 2).y) < 0.1)) {
-            goToPosition(followMe.x, followMe.y, followMe.moveSpeed, followAngle, followMe.turnSpeed);
-//        }
+        goToPosition(followMe.x, followMe.y, followMe.moveSpeed, followAngle, followMe.turnSpeed);
     }
 
     public static CurvePoint getFollowPointPath(ArrayList<CurvePoint> pathPoints, Point robotLocation, double followRadius) {
@@ -42,11 +38,6 @@ public class RobotMovement {
             CurvePoint startLine = pathPoints.get(i);
             CurvePoint endLine = pathPoints.get(i + 1);
 
-            System.out.println("robot location " + robotLocation);
-            System.out.println("followRadius " + followRadius);
-            System.out.println("startLine.toPoint()" + startLine.toPoint());
-            System.out.println("endLine.toPoint(): " +endLine.toPoint());
-
             ArrayList<Point> intersections = lineCircleIntersection(robotLocation, followRadius, startLine.toPoint(), endLine.toPoint());
 
             double closestAngle = 100000000;
@@ -54,16 +45,13 @@ public class RobotMovement {
             for(Point thisIntersection : intersections) {
                 double angle = Math.atan2(thisIntersection.y - worldYPosition, thisIntersection.x - worldXPosition);
                 double deltaAngle = Math.abs(MathFunctions.AngleWrap(angle - worldAngle_rad));
-                System.out.println("Delta angle " + deltaAngle);
+                System.out.println(STR."Delta angle\{deltaAngle}");
                 System.out.println("deltaAngle");
 
                 if(deltaAngle < closestAngle) {
                     closestAngle = deltaAngle;
                     followMe.setPoint(thisIntersection);
                 }
-//                System.out.println("allPoints: " + allPoints);
-//                lastPoint = pathPoints.get(pathPoints.size() -1);
-//                double lastSlope = (intersections.get(intersections.size() -1) - intersections.get(intersections.size() -1))
             }
             System.out.println(intersections);
 //            System.out.println("Current line: " + i + " Current closest delta angle: " + closestAngle);
